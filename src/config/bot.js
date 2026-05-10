7cf1fe5f016e59f572afcfff18c8db0a15d9a77c
tobymccool839-sys <tobymccool839@gmail.com>
2026-05-10 19:46:42 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "It's Listening",
         // Activity type number (0 = Playing).
-        type: 2, 
+        type: 3, 
       },
     ],
   },
